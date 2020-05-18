177824314964a3b65afba7b4ef0bb91e671798ff
AKSAHA <sheol.aks@gmail.com>
2020-05-18 23:45:45 +0100

    rolled back

--- test/test.js
+++ test/test.js
@@ -13,8 +13,8 @@ describe('index', function () {
     it('should return \'\' when the page is not present', function () {
         assert.equal(utils.pageQuery(), '');
     });
-    it('should return value when the page is  present', function () {
-        assert.equal(utils.pageQuery('abc'), '&page=abc');
-    });
+    // it('should return value when the page is  present', function () {
+    //     assert.equal(utils.pageQuery('abc'), '&page=abc');
+    // });
 });
 
